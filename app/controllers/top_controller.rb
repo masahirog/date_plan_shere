225fec341aa486b1006156637362e1c08b33dfa5
masahirog <masahiro11g@gmail.com>
2018-01-03 21:20:59 +0900

    second

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,6 @@
+class TopController < ApplicationController
+  def index
+  end
+  def account
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  validates :nickname,
+  uniqueness: true,
+  length: { minimum: 4, maximum: 10, message: "3文字以上10文字以下です" },
+  format: { with: /\A[a-z0-9]+\z/, message: "ユーザー名は半角英数字です"}
+
+  def to_param
+    nickname
+  end
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20],
+        image: auth.info.image,
+        name: auth.info.name,
+        nickname: auth.info.nickname
+      )
+    end
+    user
+  end
+
+  private
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
+end
